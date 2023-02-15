3e8191a62743c866682f90b6b9938ac28fbffe77
JakobRieser <jakob.rieser@t-online.de>
2023-02-15 11:00:08 +0100

    Cleaned the sgsR test bioklim script

--- R Scripts/Reference data points/sgsR test bioklim.R
+++ R Scripts/Reference data points/sgsR test bioklim.R
@@ -1,15 +1,45 @@
-library(sgsR)
-library(terra)
-library(mapview)
-library(sf)
-library(dplyr)
+## Info ----------------------------------------------------------------------------------------------------------------
+
+
+#' Author: Jakob Rieser
+#' Last updated: 2023-02-15
+#' Status: Finished
+
+
+### Purpose of script ----
+
+#' Testing the sgsR package functionalities in a real environment in the Bavarian Forest National Park
+#' Therefore, the representation of Bioklim plots is tested based on a stratification based on ALS metrics and the 
+#' land cover maps for the full national park scale
+
+
+### Notes ----
+
+#' way more analysis can be performed based on these datasets but is not implemented
+#' For more analysis see script "sgsR test BFNP"
+
+
+### Required datasets ----
+
+#' sample plots (Bioklim points)
+#' ALS metrics raster
+#' Land cover polygons
+
+
+### Required packages ----
+
+require(sgsR)
+require(terra)
+require(mapview)
+require(sf)
+require(dplyr)
 
 
 
-# 1 Import and Preprocess data -----------------------------------------------------------------------------------------
+## 1. Import and Preprocess datasets -----------------------------------------------------------------------------------
 
 
-## The ALS raster data ----
+### The ALS raster data ----
 
 #' load ALS raster representing the bioklim transects:
 ALS_metrics_bioklim <- terra::rast("C:/Data/LiDAR_metrics_201608_10m.tiff")
@@ -29,7 +59,7 @@ ALS_metrics_area <- st_as_sf(
 plot(ALS_metrics_area)
 
 
-## The Bioklim reference plots ----
+### The Bioklim reference plots ----
 
 #' load the bioklim reference plots as points:
 reference_points <- st_read("C:/Users/jakob/OneDrive/BFNP/Data/Base data/Bavarian Forest National Park/Inventory/Bioklim/LAI_TLS_plots_2017.gpkg")
@@ -39,7 +69,7 @@ mapview(reference_points)
 reference_points_transformed <- st_transform(reference_points, crs = crs(ALS_metrics_bioklim))
 
 
-## The polygon base-maps of the BFNP ----
+### The land cover polygons ("habitat maps") of the BFNP ----
 
 habitat_types <- st_read("C:/Users/jakob/OneDrive/BFNP/Data/Base data/Bavarian Forest National Park/Habitat_Types_Land_Cover.shp")
 
@@ -57,31 +87,38 @@ mapview(habitat_types_bioklim)
 habitat_types_bioklim
 
 
-# 2 Stratification -----------------------------------------------------------------------------------------------------
 
-## K-Means Classification ----
+## 2. Stratification -----------------------------------------------------------------------------------------------------
 
-#' stratification based on k-means classification 10-strata: 
+
+### K-Means classification based on the ALS metrics ----
+
+#' stratification based on k-means classification with 10-strata: 
 stratified_ALS <- strat_kmeans(mraster = ALS_metrics_bioklim, 
                                nStrata = 10, 
                                iter = 1000, 
                                algorithm = "MacQueen",
                                plot = T) 
 
+
+### Stratificytion based on the land cover polygons ----
+
 stratified_habitat_types <- strat_poly(poly = habitat_types_bioklim, attribute = "KLS_ENG")
 
-?strat_poly
 
-## Perform principal component analysis ----
+### Principal component analysis based on ALS metrics ----
+
+#' calculate PCA for 3 components
 pcomp_ALS <- calculate_pcomp(mraster = ALS_metrics_bioklim,
-                nComp = 3,
-                plot = T)
+                             nComp = 3,
+                             plot = T)
 
 
 
-# 3 Analysis -----------------------------------------------------------------------------------------------------------
+## 3. Analysis ---------------------------------------------------------------------------------------------------------
 
-## Calculate Representation ----
+
+### Calculate Representation ----
 
 #' calculate the representation of the existing reference points based on the k-means classes:
 calculate_representation(sraster = stratified_ALS,
@@ -90,7 +127,9 @@ calculate_representation(sraster = stratified_ALS,
 
 
 
-## Calculate sample size ----
+### Calculate sample size ----
+
+#' calculate the theoretical sample size needed to cover all the input datasets:
 calculate_sampsize(mraster = ALS_metrics_bioklim$BE_H_P10,
                         rse = 0.05,
                         start = 0.01,
