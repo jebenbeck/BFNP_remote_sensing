181e6d576dce2fe2fb5111ba1ee8112bbac3ce72
JakobRieser <jakob.rieser@t-online.de>
2023-12-19 17:24:25 +0100

    added support for missions containing multiple flights in the UAV preprocessing

--- R Scripts/UAV/Processing mission flight paths.R
+++ R Scripts/UAV/Processing mission flight paths.R
@@ -9,23 +9,19 @@ library(pbapply)
 file_paths <- list.files(path = "F:/UAV data/Missions/20230912_001 LiDAR LSP Spitzberg/DJI Terra project", 
                        pattern = "*_sbet.txt", recursive = T, full.names = T)
 
+#' assign index to file name:
+names(file_paths) <- c(1:length(file_paths))
 
+#' empty list to fill:
+flight_paths <- list()
 
-prepare_flight_path <- function(file_path){
+for (i in 1:length(file_paths)) {
   
   #' read in the data:
-  data <- read.table(file_path,
-                     header = F,
-                     skip = 2,
-                     numerals = "no.loss",
-                     sep = "",
-                     dec = ".")
+  data <- read.table(file_paths[[i]], header = F, skip = 2, numerals = "no.loss", sep = "", dec = ".")
   
   #' get the column names:
-  header <- read.table(file_path,
-                       header = T,
-                       nrows = 1,
-                       sep = "") %>% 
+  header <- read.table(file_paths[[i]], header = T, nrows = 1, sep = "") %>% 
     select(-1) 
   
   #' assign the new column names to the data:
@@ -41,16 +37,21 @@ prepare_flight_path <- function(file_path){
     st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, remove = F) %>% 
     # Transforming XY to EPSG:25832
     st_transform(crs = 25832) %>% 
+    #' add the coordinates to the attribute table:
     mutate(X.UTM32 = st_coordinates(.)[, 1],
            Y.UTM32 = st_coordinates(.)[, 2],
-           .after = Longitude)
+           .after = Longitude) %>% 
+    #' add flight index to the attribute table:
+    mutate(Flight.index = names(file_paths)[[i]],
+           .before = Time)
   
+  #' add data to list:
+  flight_paths[[i]] <- data_sf
+
 }
 
-test <- pblapply(file_paths, prepare_flight_path)
-
-test_full <- rbind(test)
-nrow(test_full)
+#' merge to single flight path:
+full_mission_path <- bind_rows(flight_paths)
 
 #' Output:
-st_write(data_sf, "F:/UAV data/Missions/20230912_001 LiDAR LSP Spitzberg/Results/Mission flight path.gpkg", driver = "GPKG", append = F)
+st_write(full_mission_path, "F:/UAV data/Missions/20230912_001 LiDAR LSP Spitzberg/Results/Mission flight path.gpkg", driver = "GPKG", append = F)
