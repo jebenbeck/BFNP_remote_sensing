411f0f330583901417f9421b553646cde74e027b
JakobRieser <jakob.rieser@t-online.de>
2024-09-23 23:02:08 +0200

    calculate UTM coordinates from polar in relation to center

--- R Scripts/MLS_convert_gcps_to_coord.R
+++ R Scripts/MLS_convert_gcps_to_coord.R
@@ -28,6 +28,8 @@ require(tidyverse)
 require(units)
 require(ggplot2)
 library(plotly)
+library(sf)
+library(mapview)
 
 ### Required functions and scripts ----
 
@@ -37,14 +39,16 @@ library(plotly)
 
 
 
-## 1. Calculate relative coordinates -----------------------------------------------------------------------------------
 
 input_path <- "C:/Users/jakob/OneDrive/BFNP/Data/Laserscanner Waldinventur/Collect Daten/collect-xlsx-data-export-laserscanning_waldinventur-ENTRY-2024-09-20T09_56_30/"
 
 plot_data <- read_xlsx(paste0(input_path, "aufnahme_terrestrischer_laserscanner.xlsx"))
-gcps <- read_xlsx(paste0(input_path, "gcps.xlsx"))
-inventory_plots <- "path/to/best/inv_centerpoints.gpkg"
 
+gcps <- read_xlsx(paste0(input_path, "gcps.xlsx")) %>% 
+  select(-contains("unit_name")) %>% 
+  rename("plot_id" = "aufnahme_terrestrischer_laserscanner_plot_id")
+
+gcps
 
 ### Convert the GCP coordinates ----
 
@@ -53,29 +57,39 @@ inventory_plots <- "path/to/best/inv_centerpoints.gpkg"
 deg_2_rad <- function(angle_deg) {(angle_deg * pi) / (180)}
 
 sd_2_hd <- function(angle_deg, dist_s){
-  angle_rad <- deg2rad(angle_deg)
+  angle_rad <- deg_2_rad(angle_deg)
   dist_h <- (dist_s * cos(angle_rad))
   return(dist_h)
 }
 
 gcps$gcp_dist_h <- sd_2_hd(gcps$gcp_winkel, gcps$gcp_dist)
-View(gcps)
 
 #' convert polar coordinates to relative XYZ-Coordinates:
 gcps$X_rel <- (gcps$gcp_dist_h/100)*sin(deg_2_rad(gcps$gcp_azimut))
 gcps$Y_rel <- (gcps$gcp_dist_h/100)*cos(deg_2_rad(gcps$gcp_azimut))
 gcps$Z_rel <- (gcps$gcp_dist_h/100)*tan(deg_2_rad(gcps$gcp_winkel))
-View(gcps)
 
 
-#' vusialize:
-plot_ly(x=gcps$X_rel, y=gcps$Y_rel, z=gcps$Z_rel, type="scatter3d", mode="markers")
+#' visualize coordinates for checking:
+plot_ly(x=gcps$X_rel, y=gcps$Y_rel, z=gcps$Z_rel, type="scatter3d", mode="markers", color = gcps$gcp_richtung)
 
 
-### Convert to absolute geographic coordinates:
+#' calculate absolute coordinates in UTM32: 
 
-# join the center point infor to each plot:
+#' join center coordinates:
+test <- gcps %>% 
+  left_join(select(plot_data, c("plot_id",
+                                "koordinate_x",
+                                "koordinate_y",
+                                "koordinate_altitude")), 
+            by = "plot_id") %>% 
+  mutate(X_UTM = X_rel + koordinate_x,
+         Y_UTM = Y_rel + koordinate_y,
+         Z_DHHN16 = Z_rel + koordinate_altitude)
+
+x <- st_as_sf(test, coords = c("X_UTM", "Y_UTM"), crs = "EPSG:25832", na.fail = F, remove = F)
+
+mapview(x)
+x
+# join the center point info to each plot:
 left_join(gcps, plot_data, by = c())
-
-
-
