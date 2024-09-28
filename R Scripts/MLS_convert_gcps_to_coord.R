13e0ff94361d79e24005477b835d0e3ec1f74521
JakobRieser <jakob.rieser@t-online.de>
2024-09-28 20:17:31 +0200

    implemented tidyverse for the full pipeline and added descriptions

--- R Scripts/MLS_convert_gcps_to_coord.R
+++ R Scripts/MLS_convert_gcps_to_coord.R
@@ -8,15 +8,20 @@
 
 ### Purpose of script ----
 
-#' during the laserscanning campaign along the concentric inventory circle plots, ground reference points were recorded to georeference the data
+#' during the terrestrial laserscanning campaign along the concentric inventory circle plots, ground control points 
+#' were recorded in order to georeference the data
 #' these were measured using azimut, distance and angle of each plot regarding to the center plot
 #' the center plot was recorded using the internal tablet gps and external gnss data (raw format)
 
-#' This script calculates the true UTM32 coordinates from the original measurements and exports them as csv and gpkg.
- 
+#' This script calculates the X,Y,Z coordinates relative to the center plot and absolute in UTM32
+#' The resulting data sets are exported as spatial (Geopackage) and tabular (csv) files for further usage
+
 
 ### Notes ----
 
+#' TO-DO: 
+#' - renaming all column names
+#' - export in FARO-csv-format (seperated by plot)
 
 ### Required datasets ----
 
@@ -29,6 +34,10 @@ require(ggplot2)
 require(plotly)
 require(sf)
 require(mapview)
+
+
+
+## 1. Convert the data -------------------------------------------------------------------------------------------------
 
 
 ### Required functions and scripts ----
@@ -44,69 +53,83 @@ sd_2_hd <- function(angle_deg, dist_s){
 }
 
 
-### Import data ----
+### Import required datasets ----
 
+#' define path to the data exported by OpenFOris Collect app (excel files)
 input_path <- "C:/Users/jakob/OneDrive/BFNP/Data/Laserscanner Waldinventur/Collect Daten/Export/"
 
+#' import the plot portion of the data:
 plot_data <- read_xlsx(paste0(input_path, "aufnahme_terrestrischer_laserscanner.xlsx"))
 
+#' import the gcps:
 gcps <- read_xlsx(paste0(input_path, "gcps.xlsx")) %>% 
   select(-contains("unit_name")) %>% 
   rename( "plot_id" = "aufnahme_terrestrischer_laserscanner_plot_id")
 
-gcps
+#' import the coordinates as measured by differential GNSS (external)
+plot_coordinates <- plot_data %>% 
+  select("plot_id", "koordinate_x", "koordinate_y", "koordinate_altitude") %>% 
+  rename("X_UTM_center" = "koordinate_x", 
+         "Y_UTM_center" = "koordinate_y",
+         "Z_DHHN16_center" = "koordinate_altitude") 
 
-### Convert the GCP coordinates ----
 
-#' calculate the horizontal distance:
-gcps$gcp_dist_h <- sd_2_hd(gcps$gcp_winkel, gcps$gcp_dist)
+### Pipeline to process the data ----
 
-#' convert polar coordinates to relative XYZ-Coordinates:
-gcps$X_rel <- (gcps$gcp_dist_h/100)*sin(deg_2_rad(gcps$gcp_azimut))
-gcps$Y_rel <- (gcps$gcp_dist_h/100)*cos(deg_2_rad(gcps$gcp_azimut))
-gcps$Z_rel <- (gcps$gcp_dist_h/100)*tan(deg_2_rad(gcps$gcp_winkel))
+gcps_coord <- gcps %>%
+  #' calculate the horizontal distance:
+  mutate(gcp_dist_h = sd_2_hd(gcp_winkel, gcp_dist)) %>%
+  #' convert polar coordinates to relative XYZ-Coordinates:
+  mutate(X_rel = (gcp_dist_h/100)*sin(deg_2_rad(gcp_azimut)),
+         Y_rel = (gcp_dist_h/100)*cos(deg_2_rad(gcp_azimut)),
+         Z_rel = (gcp_dist_h/100)*tan(deg_2_rad(gcp_winkel))) %>% 
+  #' join relating center point coordinate to each gcp:
+  left_join(plot_coordinates, by = "plot_id") %>% 
+  #' calculate absolute UTM coordinates by using the center point coordinates:
+  mutate(X_UTM = X_rel + X_UTM_center,
+         Y_UTM = Y_rel + Y_UTM_center,
+         Z_DHHN16 = Z_rel + Z_DHHN16_center) %>% 
+  #' remove unneccessary columns:
+  select(-c(X_UTM_center, Y_UTM_center, Z_DHHN16_center))
+
+gcps_coord         
+
+#' reformat the center coordinates to be appended to the gcp dataset:
+plot_coordinates_formatted <- plot_coordinates %>% 
+  rename("X_UTM" = "X_UTM_center",
+         "Y_UTM" = "Y_UTM_center",
+         "Z_DHHN16" = "Z_DHHN16_center") %>% 
+  mutate("_gcps_position" = 0,
+         "gcp_richtung" = "z")
+
+#' add the center coordinates to the gcp dataset as additional gcp:
+gcps_coord_full <- bind_rows(gcps_coord, plot_coordinates_formatted) %>% 
+  #' replace all NAs with 0:
+  mutate(
+    across(everything(), ~replace_na(.x, 0))
+  ) %>% 
+  #' name each gcp after its' position and plot_id:
+  mutate(gcp_name = paste0(plot_id, "_", gcp_richtung), .before = gcp_azimut) %>% 
+  #' sort the dataset:
+  arrange(plot_id, gcp_richtung)
+
+gcps_coord_full
+
+#' convert all gcps to spatial data:
+gcps_coord_full_spatial <- st_as_sf(gcps_coord_full, coords = c("X_UTM", "Y_UTM"), crs = "EPSG:25832", na.fail = F, remove = F)
+
+### Check and visualize the data ----
 
 #' visualize relative coordinates for checking:
-plot_ly(data = gcps, x = ~X_rel, y = ~Y_rel, z = ~Z_rel, type="scatter3d", mode="markers", color = gcps$gcp_richtung, 
+plot_ly(data = gcps_coord, x = ~X_rel, y = ~Y_rel, z = ~Z_rel, type="scatter3d", mode="markers", color = gcps$gcp_richtung, 
         text = ~paste("ID:", plot_id, "<br>", 
                       "X:", X_rel, "<br>", 
                       "Y:", Y_rel, "<br>", 
                       "Z:", Z_rel),
         hoverinfo = "text") 
 
-#' calculate absolute UTM coordinates by using the center point coordinates:
-gcps_koord <- gcps %>% 
-  left_join(select(plot_data, c("plot_id",
-                                "koordinate_x",
-                                "koordinate_y",
-                                "koordinate_altitude")), 
-            by = "plot_id") %>% 
-  mutate(X_UTM = X_rel + koordinate_x,
-         Y_UTM = Y_rel + koordinate_y,
-         Z_DHHN16 = Z_rel + koordinate_altitude)
-
-#' select center coordinates and append them to the gcps:
-plot_data_coord <- select(plot_data, c("plot_id",
-                                       "koordinate_x",
-                                       "koordinate_y",
-                                       "koordinate_altitude")) %>% 
-  rename("X_UTM" = "koordinate_x",
-         "Y_UTM" = "koordinate_y",
-         "Z_DHHN16" = "koordinate_altitude") %>% 
-  mutate("_gcps_position" = 0,
-         "gcp_richtung" = "z")
-
-gcps_coord_full <- coord <- bind_rows(gcps_koord, plot_data_coord) %>% 
-  #' name the point data to their real names:
-  mutate(Name = paste0(plot_id, "_", gcp_richtung), .before = gcp_azimut) %>% 
-  arrange(plot_id, gcp_richtung)
-gcps_coord_full
-
-
-#' convert to spatial data:
-sf <- st_as_sf(gcps_coord_full, coords = c("X_UTM", "Y_UTM"), crs = "EPSG:25832", na.fail = F, remove = F)
-
-mapview(sf, label = sf$Name, zcol = "gcp_richtung")
+#' visualize spatial dataset:
+mapview(gcps_coord_full_spatial, label = gcps_coord_full_spatial$gcp_name, zcol = "gcp_richtung")
 
 
 ### Export data ----
