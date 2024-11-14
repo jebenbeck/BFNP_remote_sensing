087563a979940727b15734d64291fba46bc0a78f
Jakob Ebenbeck <jakob@ebenbeck.de>
2024-11-14 09:18:22 +0100

    add the normalized height to the las file output assign crs to las

--- R Scripts/ALS data processing/ALS data normalization.R
+++ R Scripts/ALS data processing/ALS data normalization.R
@@ -3,8 +3,8 @@ library(lidR)
 
 #' read las file
 #' it should have classified ground points:
-las <- readLAS(system.file("extdata", "Topography.laz", package = "lidR"))
-
+las <- readLAS("H:/Daten ADA/ALS_20230715_Scheuereck.las")
+las
 #' normalize point cloud based on DTM:
 las_normalized <- lidR::normalize_height(las, algorithm = tin())
 
@@ -13,15 +13,21 @@ temp <- las_normalized$Z
 
 # Step 2: Switch Z and Zref
 las_normalized$Z <- las_normalized$Zref
-las_normalized$Zref <- temp
 
-#' check:
-las_normalized$Zref
-las_normalized$Z
-las_normalized@data
-head(las_normalized@data)
+#' add the normalized Z values as a proper attribute for export:
+las_output <- add_lasattribute(las_normalized, x = temp, name = "Z_norm", desc = "Normalize Height")
+
+#' add correct coordinate system:
+st_crs(las_output) <- 25832
 
 #' Visualize:
 plot(las_normalized, color = "Z")
 plot(las_normalized, color = "Zref")
 
+#' export:
+writeLAS(las_output, "H:/Daten ADA/ALS_20230715_Scheuereck_normalized.las")
+
+#' check:
+las_2 <- readLAS("H:/Daten ADA/ALS_20230715_Scheuereck_normalized.las")
+las_2@data
+las_2
