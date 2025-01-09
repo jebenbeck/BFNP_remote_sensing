663225a823eea36a584681f957ac2772a65844c5
Jakob Ebenbeck <jakob.ebenbeck@npv-bw.bayern.de>
2025-01-09 14:28:17 +0100

    implemented function to download rasterdb data from RSDB server in tiles

--- RSDB/RSDB query rasters and vectors.R
+++ RSDB/RSDB query rasters and vectors.R
@@ -12,8 +12,7 @@ library(pbapply)
 # ---- Connect to RSDB server ---- #
 
 #' Provide the login-credentials in an local R file on your computer or via an object (format: "username:password")
-source("C:/Users/Rieser/OneDrive/BFNP/Documents/RSDB/RSDB credentials.R")
-
+credentials <- "username:password"
 
 #' Connect to the server
 db <- RemoteSensing$new("https://foresteye-server.de:8082", credentials) 
@@ -52,38 +51,84 @@ terra::writeRaster(ALS_metrics_prj.rast, "G:/ALS_metrics_2017_10m.tif", overwrit
 ### 1.2. Large datasets ----
 
 #' sometimes, the data is too large to be stored or downloaded in a single *.tif file and should be downloaded and/or 
-#' stored in tiles instead. The following code cuts the AOI into tiles and downloads them one by one. 
+#' stored in tiles instead. The following function cuts the AOI into tiles and downloads them one by one. 
 
-#' Convert the extent object to a SpatialPolygons and then to an sf object
-AOI.sf <- as(AOI, "SpatialPolygons") %>% 
-  st_as_sf(AOI.sp)
+#' definition of inputs:
+#' 1) rasterdb_name is the character name of the rasterdb that should be downloaded
+#' 2) AOI can be the character string "full" which returns the full extent of the rasterdb or a spatial polygon 
+#' object (type st/sf) resulting from gpkg or shp files. In this case the path to the file needs to be pasted here.
+#' 3) tilesize is the length of the tile sides in x and y directions corresponding to mapping units (usually meters)
+#' 4) bands can be one number or a vector of numbers corresponding to the bands/layers of the rasterdb that should be downloaded 
 
-#' Set projection to match the source:
-st_crs(AOI.sf) <- ALS_metrics.db$geo_code
-
-#' Cutting the AOI into tiles of 1 km², any size can be chosen (distance of side length in mapping units): 
-AOI_tiles.sf <- st_make_grid(AOI.sf, 1000) %>% 
-  st_as_sf()
-
-#' Function iterating through all tiles and downloading the respective data:
-RSDB_query_raster_tiles <- function(i){
-  #' make the respective tile new AOI:
-  AOI <- raster::extent(AOI_tiles.sf[i,])
-  #' Query the data:
-  raster_data <- ALS_metrics.db$raster(ext = AOI, band = 3)
-  #' convert to rast:
-  return(rast(raster_data))
+RSDB_query_raster_tiles <- function(rasterdb_name, AOI, tilesize, bands) {
+  
+  #' get the rasterdatabase:
+  rasterdatabase <- db$rasterdb(rasterdb_name)
+  
+  # get area of interest in correct format:
+  get_area_of_interest <- function(AOI, rasterdatabase) {
+    if (AOI == "full") {
+      # Return the extent of the raster database
+      areaofinterest <- as(rasterdatabase$extent, "SpatialPolygons") %>% 
+        st_as_sf(AOI.sp)
+      st_crs(areaofinterest) <- rasterdatabase$geo_code
+    } else {
+      # Otherwise, assume AOI is a polygon object (sf object)
+      areaofinterest <- st_read(AOI, quiet = T)
+    }
+    
+    return(areaofinterest)
+  }
+  
+  area_of_interest <- get_area_of_interest(AOI, rasterdatabase)
+  
+  #' Cutting the AOI into tiles of 1 km², any size can be chosen (distance of side length in mapping units): 
+  tiles <- st_make_grid(area_of_interest, tilesize) %>% 
+    st_as_sf()
+  
+  print(paste0("tiling successfull, ", nrow(tiles), " tiles generated"))
+  
+  #' Function iterating through all tiles and downloading the respective data:
+  query_tiles <- function(i){
+    #' make the respective tile new AOI:
+    area_of_interest_tile <- raster::extent(tiles[i,])
+    #' Query the data:
+    raster_data <- rast(rasterdatabase$raster(ext = area_of_interest_tile, band = bands))
+    #' set coordinate system:
+    terra::crs(raster_data) <- rasterdatabase$geo_code
+    
+    return(raster_data)
+  }
+  
+  print("starting downloading tiles from RSDB server...")
+  
+  #' Applying the function with bar tracking the progress:
+  raster_tiles <- pblapply(1:nrow(tiles), query_tiles)
+  
+  print("download successfull")
+  
+  return(raster_tiles)
+  
 }
 
-#' Applying the function with bar tracking the progress:
-ALS_metrics_tiles.rast <- pblapply(1:nrow(AOI_tiles.sf), RSDB_query_raster_tiles)
-ALS_metrics_tiles.rast
+#' run function to receive list of rasters in memory:
+raster_list <- RSDB_query_raster_tiles (rasterdb_name = "LiDAR_metrics_ALS_2023_07_10m", AOI = "full", 
+                                        tilesize = 1000, bands = 3)
 
-#' Merging the rasters:
-ALS_metrics_merged.rast <- do.call(merge, ALS_metrics_tiles.rast)
+#' Export the tiles to disk as seperate tif files:
+pblapply(seq_along(raster_list), function(i) {
+  # Construct the filename
+  filename <- file.path("C:/Users/NBW-Ebenbeck_J/Downloads/", paste0("LiDAR_metrics_2023_07_10m_", i, ".tif"))
+  
+  # Export the raster object to a .tif file
+  terra::writeRaster(raster_list[[i]], filename, overwrite = TRUE)
+})
 
-#' Export to disk:
-terra::writeRaster(ALS_metrics_merged.rast, "G:/ALS_metrics_2023_10m.tif", overwrite = T)
+#' Merging the tiles to single raster file:
+raster_merged <- do.call(merge, raster_tiles)
+
+#' Export the merged raster to tif:
+terra::writeRaster(raster_list[[i]], filename, overwrite = TRUE)
 
 
 
