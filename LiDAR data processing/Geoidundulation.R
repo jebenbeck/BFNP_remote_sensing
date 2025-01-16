aeca401fc1876aec2aa8ca82da617f5cce04751f
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-01-16 09:36:11 +0100

    made the geoidundulation of ALS files a bit more precise

--- LiDAR data processing/Geoidundulation.R
+++ LiDAR data processing/Geoidundulation.R
@@ -4,36 +4,43 @@ library(lidR)
 library(mapview)
 library(sf)
 
+### Preprocess the geoid ----
+
 # Read the geoid model (TIFF file)
-geoid <- rast("C:/Users/NBW-Ebenbeck_J/Downloads/quasigeoid.geo89.de/quasigeoid.geo89.de/quasigeoid/de/windows/geotiff/GCG2016v2023.tif")
+geoid <- rast("C:/Users/NBW-Ebenbeck_J/Downloads/quasigeoid.geo89.de/quasigeoid/de/windows/geotiff/GCG2016v2023.tif")
 plot(geoid)
 
-#' reproject geoid to match point cloud:
-geoid_UTM <- project(geoid, "epsg:25832")
+poly <- as.polygons(geoid, values = F, aggregate = T) 
+poly
 
-# Define the extent (in UTM zone 32N coordinates)
-ext <- ext(450000, 550000, 5500000, 5600000)
+poly_merged <- terra::aggregate(poly)
+mapview(poly_merged)
+#' Interpolate for higher resolution and smoother results:
 
-# Define the cell size (resolution) in meters
-cell_size <- c(30, 30)  # 100 meters by 100 meters
-
-# Define the CRS as EPSG:25832 (UTM zone 32N)
-crs_epsg <- "EPSG:25832"  # UTM zone 32N
-
-# Create the empty raster
-r <- rast(ext, resolution = cell_size, crs = crs_epsg)
+# Create empty target raster
+r <- rast(x = ext(geoid), resolution = 100, crs = terra::crs(geoid))
 
 #' interpolate grid:
-y <- resample(geoid_UTM, r, method = "bilinear")
-mapview(y)
+y <- resample(geoid, r, method = "bilinear")
+
+#' reproject geoid to match point cloud:
+geoid_UTM <- project(y, "epsg:25832")
+
 
 #' export raster:
-writeRaster(y, "C:/Users/NBW-Ebenbeck_J/Downloads/quasigeoid.geo89.de/quasigeoid.geo89.de/quasigeoid/de/windows/geotiff/GCG2016v2023_30m.tif")
+writeRaster(y, "C:/Users/NBW-Ebenbeck_J/Downloads/quasigeoid.geo89.de/quasigeoid/de/windows/geotiff/GCG2016v2023_100m.tif")
 
-ALS <- readALSLAS("D:/ALS 2017/NPV_02885.laz")
+### Perform the geoidundulation on ALS data ----
+
+ALS <- readALSLAS("D:/ALS 2017/NPV_00551.laz")
+ALS
 crs(ALS) <- "EPSG:25833"
 
-ALS_trans = st_transform(ALS, "epsg:25832")
+ALS_trans <- st_transform(ALS, "epsg:25832")
+ALS_trans@data
 
-ALS_Geoid <- merge_spatial(ALS_trans, y, "Geoid")
-ALS_Geoid@data$Geoid
+ALS_Geoid <- merge_spatial(ALS_trans, y, "Geoidundulation")
+ALS_Geoid@data
+
+test <- readALSLAS("D:/ALS 2012/UTM32/spur00001_UTM.laz")
+
