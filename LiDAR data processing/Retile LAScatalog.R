59a68342100dae0bc3daf29afd45f039054db2b3
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-01-16 21:55:51 +0100

    introduced function to calculate statistics on Lascatalogs

--- LiDAR data processing/Retile LAScatalog.R
+++ LiDAR data processing/Retile LAScatalog.R
@@ -29,6 +29,7 @@ library(lidR)
 library(sf)
 library(tidyverse)
 library(stringr)
+library(mapview)
 
 
 
@@ -40,55 +41,85 @@ ctg <- readALSLAScatalog("D:/ALS 2012/test")
 
 #' check LAScatalog vailidity:
 las_check(ctg)
+summary(ctg)
 plot(ctg, mapview = TRUE)
 
+#' set catalog options:
+opt_chunk_alignment(ctg) <- c(0, 0) #' define tile alignment
+opt_chunk_size(ctg) <- 1000         #' define tile size
+opt_chunk_buffer(ctg) <- 0          #' define buffer size
+opt_laz_compression(ctg) <- T       #' define outpt as LAZ
+opt_output_files(ctg) <- paste0("D:/ALS 2012/test2", "/{XLEFT}_{YBOTTOM}") #' label outputs based on coordinates
 
-opt_chunk_alignment(ctg) <- c(0, 0)
-opt_chunk_size(ctg) <- 1000
-opt_chunk_buffer(ctg) <- 0
-plot(ctg, mapview = T)
-opt
+#' perform the retiling:
+ctg_retiled <- catalog_retile(ctg) 
 
-opt_laz_compression(ctg) <- T
-opt_output_files(ctg) <- paste0("D:/ALS 2012/test2", "/{XLEFT}_{YBOTTOM}") # label outputs based on coordinates
-ctg_retiled <- catalog_retile(ctg) # apply retile
-
+#' have a look at the new tiles:
 plot(ctg_retiled, mapview = T)
-plot(ctg, mapview = T)
 
 
 ## 2. Post-processing spatial polygons of the lascatalog ---------------------------------------------------------------
 
-
 #' postprocessing polygon data:
-ctg_polygons <- st_as_sf(ctg_retiled) %>% 
-  mutate(tile.name = str_extract(filename, "[^\\\\]+(?=\\.laz$)"), .before = everything()) %>% 
-  select(-c(File.Signature, File.Source.ID, File.Creation.Year, File.Creation.Day.of.Year, GUID, Version.Major, Version.Minor, System.Identifier,
-            Generating.Software, Header.Size, Offset.to.point.data, Number.of.variable.length.records, Point.Data.Format.ID, Point.Data.Record.Length,
-            filename))
+catalog_to_polygons <- function(clg) {
+  ctg_polygons <- st_as_sf(clg) %>% 
+    #' convert filename to tile name:
+    mutate(Tile.name = str_extract(filename, "[^\\\\]+(?=\\.laz$)"), .before = everything()) %>% 
+    #' remove unecessary attributes:
+    select(-c(File.Signature, File.Source.ID, File.Creation.Year, File.Creation.Day.of.Year, GUID, Version.Major, Version.Minor, System.Identifier,
+              Generating.Software, Header.Size, Offset.to.point.data, Number.of.variable.length.records, Point.Data.Format.ID, Point.Data.Record.Length,
+              filename)) %>% 
+    #' rename number of points:
+    rename("Number.of.points" = "Number.of.point.records") %>% 
+    relocate(CRS, .after = Tile.name) %>% 
+    relocate(Number.of.points, .before = Number.of.1st.return)
+  
+  return(ctg_polygons)
+}
+
+#' apply function:
+ctg_retiled_polygons <- catalog_to_polygons(ctg_retiled)
+
 
 #' export polygon file to geopackage:
 st_write(ctg_polygons, dsn = "D:/ALS 2012/test2/tiles.gpkg", layer = "ALS_2012-06" , append = T)
 
 
-### calculate point density of each file ----
+## 3. Calculate point density of new tiles -----------------------------------------------------------------------------
 
-opt_select(ctg_retiled) <- "xyz"
+
+#' set catalog options:
+opt_select(ctg_retiled) <- "xy" #' read only xy because they are the only attributes of interest
+opt_output_files(ctg_retiled) <- "" #' receive results in R environment 
 
 #' function to get point density of las data:
-catalog_density = function(chunk) {
+catalog_file_stats = function(chunk) {
+  #return(chunk)
   las <- readLAS(chunk)
   if (is.empty(las)) return(NULL)
-  density = density(las)
-  return(density)
+  #' calculate point density:
+  density = density(las) 
+  #' calculate area covered by convex hull (not bbox):
+  area = area(las)
+  #' extract tile name from file (for merging information to other data later):
+  tilename = str_extract(chunk@files, "[^\\\\]+(?=\\.laz$)")
+  #' extract extent of full tile:
+  extent_tile <- chunk@bbox
+  #' make data frame with necessary information:
+  df <- tibble(Tile.name = tilename, 
+               Point.density = density, 
+               Area.covered = area,
+               Tile.max.X = extent_tile[1, 2],
+               Tile.min.X = extent_tile[1, 1],
+               Tile.max.Y = extent_tile[2, 2],
+               Tile.min.Y = extent_tile[2, 1])
+  
+  return(df)
 }
 
-#' define parallel computation:
-plan(multisession, workers = 2)
-
 #' apply function to catalog:
-density = catalog_apply(ctg_retiled, catalog_density)
+catalog_stats <- catalog_apply(ctg_retiled, catalog_file_stats)
+catalog_stats_combined <- 
 
-test <- summary(ctg_retiled)
-test
-x <- density(ctg_retiled)
+ctg_retiled_polygons_joined <- ctg_retiled_polygons %>% 
+  left_join(df2, by = "Tile.name")
