fa7c06d286b99d3595658076691e954a5d642344
JakobRieser <jakob.rieser@t-online.de>
2025-02-04 10:44:32 +0100

    minor changes for testing

--- LiDAR data processing/Reproject LAS files.R
+++ LiDAR data processing/Reproject LAS files.R
@@ -4,13 +4,8 @@ library(mapview)
 library(tidyverse)
 library(future)
 
-## 1. Try different approaches -----------------------------------------------------------------------------------------
 
-
-
-## 2.Implemented into Lascatalog ---------------------------------------------------------------------------------------
-
-### 2.1. Make general setting for sf to be able to use the correct transformation method ----
+## Define transformation method ----------------------------------------------------------------------------------------
 
 #' check set up of sf package:
 sf_proj_search_paths()
@@ -25,10 +20,11 @@ View(options)
 
 #' select correct pipeline:
 pipeline_BETA2007 <- options[1,]$definition
-pipeline_BETA2007_own <- "+proj=utm +zone=32 +datum=ETRS89 +grids='D:/Reproject ALS Data test/Transformation grids/BETA2007/de_adv_BETA2007.tif'"
+pipeline_BETA2007_own <- "+proj=utm +zone=32 +datum=ETRS89 +grids='F:/Reproject ALS Data test/Transformation grids/BETA2007/de_adv_BETA2007.tif'"
+pipeline_KANU <- "+proj=utm +zone=32 +datum=ETRS89 +grids='F:/Reproject ALS Data test/Transformation grids/KANU/kanu_ntv2_niederbayern.tif'"
 
 #' read Lascatalog:
-ctg <- readALSLAScatalog("D:/Reproject ALS Data test/LiDAR GK")
+ctg <- readALSLAScatalog("F:/Reproject ALS Data test/LiDAR GK")
 
 #' check LAScatalog vailidity:
 ctg
@@ -41,8 +37,7 @@ plot(ctg, mapview = TRUE)
 
 #' define output location and structure of catalog:
 
-
-opt_output_files(ctg) <- "D:/Reproject ALS Data test/LiDAR UTM/Catalog output/{ORIGINALFILENAME}_UTM_own"
+opt_output_files(ctg) <- "F:/Reproject ALS Data test/LiDAR UTM/Test_Tower/{ORIGINALFILENAME}_UTM"
 opt_laz_compression(ctg) <- TRUE
 opt_chunk_buffer(ctg) <- 0
 opt_chunk_size(ctg) <- 0
@@ -50,12 +45,20 @@ opt_chunk_size(ctg) <- 0
 #' function to reproject las data:
 reproject_catalog = function(las)
 {
-  las_trans = st_transform(las, crs = 25832, pipeline = pipeline_BETA2007_own)
+  las_trans = sf::st_transform(las, crs = 25832, pipeline = pipeline_BETA2007)
   return(las_trans)
 }
 
 #' define parallel computation:
-plan(multisession, workers = 6)
+#plan(multisession, workers = 6)
 
 #' apply function to catalog:
 reprojected_ctg = catalog_map(ctg, reproject_catalog)
+
+
+
+GK_single_file <- readLAS("F:/Reproject ALS Data test/LiDAR GK/2017_Trinkwassertalsperre.laz")
+
+reprojected_single_file <- st_transform(GK_single_file, crs = 25832, pipeline = pipeline_BETA2007) 
+
+writeLAS(reprojected_single_file, "F:/Reproject ALS Data test/LiDAR UTM/Test_Tower/test.laz")
