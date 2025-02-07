8f71dd03220aa38d27ed6cc9139ed565777339f1
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-02-07 11:38:15 +0100

    some more tests

--- Miscellaneous/Transform data from GK4 to UTM32.R
+++ Miscellaneous/Transform data from GK4 to UTM32.R
@@ -4,12 +4,6 @@ library(sf)
 library(mapview)
 library(tidyverse)
 
-#' check set up of sf package:
-sf_proj_search_paths()
-sf_extSoftVersion()
-sf_proj_network(enable = T)
-sf_proj_network()
-
 #' get ground reference points data in GK and UTM:
 reference_points <- read.csv("D:/Reproject ALS Data test/Transformation grids/KANU/Testpunkte_Echtumstellung.csv", sep = ";") %>% 
   rename("UTM32_X" = "E.UTM32",
@@ -17,24 +11,49 @@ reference_points <- read.csv("D:/Reproject ALS Data test/Transformation grids/KA
          "GK4_X" = "R.GK4.",
          "GK4_Y" = "H.GK4.")
 
-rp_UTM <- st_as_sf(reference_points, coords = c("UTM32_X", "UTM32_Y"), crs = st_crs(25832), remove = F)
 rp_GK <- st_as_sf(reference_points, coords = c("GK4_X", "GK4_Y"), crs = st_crs(31468), remove = F)
+
+mapview(rp_GK)
+
 
 ## Transformations -----------------------------------------------------------------------------------------------------
 
 
+### Without network connection (default bahavior) ----
+
+
+#' check set up of sf package:
+sf_proj_search_paths()
+sf_extSoftVersion()
+sf_proj_network() #' should be FALSE, if not run: sf_proj_network(enable = F)
+
+#' run default transformation:
+rp_transformed_default <- st_transform(rp_GK, crs = 25832)
+
+
+### With network connection enabled ----
+
+
+#' connect to proj library:
+sf_proj_network(enable = T)
+
 #' get transformation options available with grid:
 options <- sf_proj_pipelines(source_crs = "EPSG:31468", target_crs = "EPSG:25832")
+View(options)
 
-pipeline_BETA2007 <- options[1,]$definition
+#' run new default transformation:
+rp_transformed_default_network <- st_transform(rp_GK, crs = 25832)
 
+#' select BETA2007 transformation options:
+pipeline_BETA2007 <- "+proj=pipeline +step +inv +proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +step +proj=hgridshift +grids=de_adv_BETA2007.tif +step +proj=utm +zone=32 +ellps=GRS80"
 
 #' apply different transformations:
-
-#' default:
-rp_transformed_default <- st_transform(rp_GK, crs = 25832)
-#' Beta2007 definition:
 rp_transformed_BETA2007 <- st_transform(rp_GK, pipeline = pipeline_BETA2007)
+
+
+
+## Calculate the accurracy -----------------------------------------------------------------------------------------------------
+
 
 #' calculate the accurracy of the different transformations:
 calculate_accurracy <- function(rp_transformed, transformation_name){
@@ -49,7 +68,7 @@ calculate_accurracy <- function(rp_transformed, transformation_name){
   }
 
 accurracy_default <- calculate_accurracy(rp_transformed_default, "default")
-accurracy_beta2007 <- calculate_accurracy(rp_transformed_default, "beta2007")
+accurracy_default_network <- calculate_accurracy(rp_transformed_default_network, "default_network")
 
 boxplot(accurracy_default)
-boxplot(accurracy_beta2007)
+boxplot(accurracy_default_network)
