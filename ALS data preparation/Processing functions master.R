083a7a981168a0939eb52380683e1e9e97ac2b9d
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-02-12 17:24:32 +0100

    pushed some ALS processing functions into the functions master file for external use

--- ALS data preparation/Processing functions master.R
+++ ALS data preparation/Processing functions master.R
@@ -1,5 +1,5 @@
 
-## Reproject LasCatalog ------------------------------------------------------------------------------------------------
+## 1. Reproject LasCatalog ------------------------------------------------------------------------------------------------
 
 
 #' This is a function to transform the coordinate system of a lascatalog. It is specialized for large-scale ALS data (many flight lines / tiles).
@@ -18,7 +18,7 @@ reproject_lascatalog <- function(lascatalog, input_epsg, output_epsg, output_pat
   }
   
   #' apply options to lascatalog
-  opt_output_files(lascatalog) <- paste0(output_path, "/{ORIGINALFILENAME}_transformed")
+  opt_output_files(lascatalog) <- paste0(output_path, "/{ORIGINALFILENAME}")
   opt_laz_compression(lascatalog) <- TRUE
   opt_chunk_buffer(lascatalog) <- 0
   opt_chunk_size(lascatalog) <- 0
@@ -57,4 +57,94 @@ reproject_lascatalog <- function(lascatalog, input_epsg, output_epsg, output_pat
   #' apply function to lascatalog:
   reprojected_catalog = catalog_map(lascatalog, reproject)
   return(reprojected_catalog)
+}
+
+
+
+## 2. Generate footprint polygons of lascatalog ------------------------------------------------------------------------
+
+
+catalog_to_polygons <- function(clg) {
+  ctg_polygons <- st_as_sf(clg) %>% 
+    #' convert filename to tile name:
+    mutate(Tile.name = str_extract(filename, "[^\\\\]+(?=\\.laz$)"), .before = everything()) %>% 
+    #' remove unecessary attributes:
+    select(-c(File.Signature, File.Source.ID, File.Creation.Year, File.Creation.Day.of.Year, GUID, Version.Major, Version.Minor, System.Identifier,
+              Generating.Software, Header.Size, Offset.to.point.data, Number.of.variable.length.records, Point.Data.Format.ID, Point.Data.Record.Length,
+              filename)) %>% 
+    #' rename number of points:
+    rename("Number.of.points" = "Number.of.point.records") %>% 
+    #' restructure:
+    relocate(CRS, .after = Tile.name) %>% 
+    relocate(Number.of.points, .before = Number.of.1st.return)
+  
+  return(ctg_polygons)
+}
+
+
+
+## 3. Calculate point density of lascatalog files ----------------------------------------------------------------------
+
+catalog_statistics <- function(lascatalog){
+  
+  #' set catalog options:
+  opt_select(lascatalog) <- "xy" #' read only xy because they are the only attributes of interest
+  opt_output_files(lascatalog) <- "" #' receive results in R environment 
+  opt_independent_files(lascatalog) <- TRUE
+  
+  #' function to get point density of las data:
+  calc_statistics = function(chunk) {
+    #return(chunk)
+    las <- readLAS(chunk)
+    if (is.empty(las)) return(NULL)
+    #' calculate point density:
+    density = density(las) 
+    #' calculate area covered by convex hull (not bbox):
+    area = area(las)
+    #' extract tile name from file (for merging information to other data later):
+    tilename = str_extract(chunk@files, "[^\\\\]+(?=\\.laz$)")
+    #' extract extent of full tile:
+    extent_tile <- chunk@bbox
+    #' make data frame with necessary information:
+    df <- tibble(Tile.name = tilename, 
+                 Point.density = density, 
+                 Area.covered = area,
+                 Tile.max.X = extent_tile[1, 2],
+                 Tile.min.X = extent_tile[1, 1],
+                 Tile.max.Y = extent_tile[2, 2],
+                 Tile.min.Y = extent_tile[2, 1])
+    
+    return(df)
+  }
+  
+  #' perform the calculation:
+  statistics <- catalog_apply(lascatalog, calc_statistics)
+  #' merge the results to a single data frame:
+  statistics_merged_df <- bind_rows(statistics)
+}
+
+
+
+## 4. Retiling ---------------------------------------------------------------------------------------------------------
+
+
+#' This function retiles lascatalogs, so many las files corresponding to a single acquisition. Sometimes, ALS las data is 
+#' not stored in rigid tiles but in parts of flight lines which can overlap. This is not good, because if you want to 
+#' know all points over a certain location within your AOI, you need to consider multiple files to get all of them.
+#' 
+#' This script takes any lascatalog (directory with many las/laz files) and restructures it to match a common grid of 
+#' 1x1 km tiles in the coordinate system.
+
+
+catalog_retiling <- function(lascatalog, tile_alignment = c(0,0), tile_size = 1000, buffer_size = 0, output_path, 
+                             filename_convention = "{XLEFT}_{YBOTTOM}", laz_compression = T){
+  #' set catalog options:
+  opt_chunk_alignment(lascatalog) <- tile_alignment
+  opt_chunk_size(lascatalog) <- tile_size 
+  opt_chunk_buffer(lascatalog) <- buffer_size
+  opt_laz_compression(lascatalog) <- laz_compression
+  opt_output_files(lascatalog) <- paste0(output_path, "/", filename_convention) 
+  
+  #' perform the retiling:
+  ctg_retiled <- catalog_retile(lascatalog) 
 }
