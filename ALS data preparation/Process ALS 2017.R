02143fa2955c43932a76dfe4f3d3734fa1c50b5c
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-02-27 12:57:43 +0100

    Evaluating the accurracy of the 2017 ALS data

--- ALS data preparation/Process ALS 2017.R
+++ ALS data preparation/Process ALS 2017.R
@@ -118,3 +118,24 @@ ctg_UTM32_AOIs <- catalog_clip_polygons(ctg_UTM32_retiled, input_epsg = "EPSG:25
 
 
 
+## 5. Accurracy evaluation ---------------------------------------------------------------------------------------------
+
+
+#' load AOIs:
+AOIs <- st_read("F:/Reproject ALS Data test/AOIs.gpkg", layer = "AOIs_UTM")
+
+#' prepare the GCPs as generated using CloudCompare:
+GCPs_2017 <- prepare_GCPs(input_path = "F:/Reproject ALS Data test/ALS data/ALS 2017/GCPs",
+                     output_path = "F:/Reproject ALS Data test/ALS data/ALS 2017/GCPs",
+                     filename = "GCPs_ALS_2017", polygons = AOIs)
+
+
+#' merge the GCPs with the reference GCPs:
+GCPs_ref <- st_read("F:/Reproject ALS Data test/ALS data/ALS 2019/GCPs/GCPs_ALS_2019.gpkg")
+GCPs_2017 <- st_read("F:/Reproject ALS Data test/ALS data/ALS 2017/GCPs/GCPs_ALS_2017.gpkg")
+
+GCPs_2017_ref <- merge_GCPs(GCPs_2017, GCPs_ref, export = T, filename = "GCPs_ALS_2017_ref", output_path = "F:/Reproject ALS Data test/ALS data/ALS 2017/GCPs")
+
+#' generate boxplots:
+aa_create_boxplots(gcp_data = GCPs_2017_ref, export = T, filename = "Difference_GCPs_ALS_2017",
+                   output_path = "F:/Reproject ALS Data test/ALS data/ALS 2017/GCPs")
