880f560354b0fc42f69dd76da7c3a9737bf3fc95
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-02-27 08:59:31 +0100

    first draft to compare GCPs from 2 different ALS data sets

--- ALS data preparation/Accurracy assessment.R
+++ ALS data preparation/Accurracy assessment.R
@@ -1,6 +1,7 @@
 library(sf)
 library(tidyverse)
 library(mapview)
+library(ggplot2)
 
 #' Accurracy assessment 
 
@@ -10,7 +11,7 @@ AOIs$name
 
 #' preprocessing the GCPs derived from ALS point clouds in cloudcompare:
 
-file_list <- list.files("H:/Reproject ALS Data test/ALS data/ALS 2019/GCPs", pattern = "\\.txt$", full.names = T)
+file_list <- list.files("H:/Reproject ALS Data test/ALS data/ALS 2017/GCPs", pattern = "\\.txt$", full.names = T)
 
 # Read all files into a list using read.table
 data <- lapply(file_list, read.table, header = F, sep = ",") %>% 
@@ -25,5 +26,32 @@ data <- lapply(file_list, read.table, header = F, sep = ",") %>%
 data
 
 #' export to disk:
-st_write(data, "H:/Reproject ALS Data test/ALS data/ALS 2019/GCPs/GCPs_ALS_2019.gpkg")
+st_write(data, "H:/Reproject ALS Data test/ALS data/ALS 2019/GCPs/GCPs_ALS_2017.gpkg")
+
+#' compare:
+
+GCPs_ref <- st_read("H:/Reproject ALS Data test/ALS data/ALS 2019/GCPs/GCPs_ALS_2019.gpkg")
+GCPs_2017 <- st_read("H:/Reproject ALS Data test/ALS data/ALS 2017/GCPs/GCPs_ALS_2017.gpkg")
+
+nearest_indices <- st_nearest_feature(GCPs_ref, GCPs_2017)
+
+# Add corresponding matches from sf2 to sf1
+GCPs_ref$matchedID <- GCPs_2017$ID[nearest_indices]
+
+# Join attributes
+merged_sf <- left_join(as.data.frame(GCPs_ref), as.data.frame(GCPs_2017), by = c("matchedID" = "ID"), suffix = c("_ref", "_test")) %>% 
+  mutate(deltaX = X_ref - X_test) %>% 
+  mutate(deltaY = Y_ref - Y_test) %>%
+  mutate(deltaZ = Z_ref - Z_test) %>%
+  filter(abs(deltaX) < 5)
+
+ggplot(data = merged_sf, aes(y = deltaX, x = AOI_ref)) +
+  geom_boxplot()
+
+ggplot(data = merged_sf, aes(y = deltaY, x = AOI_ref)) +
+  geom_boxplot()
+
+ggplot(data = merged_sf, aes(y = deltaZ, x = AOI_ref)) +
+  geom_boxplot()
+
 
