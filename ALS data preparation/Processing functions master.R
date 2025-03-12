72ff6aae90765b51b8968aabcdc2f19feb521b8c
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-03-12 09:39:55 +0100

    implemented first tests for ground point classification and normalization of ALS data into the function master

--- ALS data preparation/Processing functions master.R
+++ ALS data preparation/Processing functions master.R
@@ -330,3 +330,70 @@ aa_metrics <- function(gcp_data, export = F, filename, output_path){
   return(summary_table)
   
 }
+
+
+
+## 7. Ground point classification --------------------------------------------------------------------------------------
+
+
+
+#' just a test
+
+#' Make LAScatalog object:
+ctg <- readLAScatalog("E:/02_Punktwolke")
+
+#' check LAScatalog vailidity:
+las_check(ctg)
+
+#' plot LAScatalog:
+plot(ctg, mapview = TRUE)
+
+#' define output location and file structure:
+opt_output_files(ctg) <- "C:/ALS Data/Classification Output/{ORIGINALFILENAME}_classified"
+opt_laz_compression(ctg) <- TRUE
+
+#' define parallel computation:
+plan(multisession, workers = 2L)
+
+#' classify ground points:
+out <- classify_ground(ctg, algorithm = csf(), last_returns = T)
+
+
+
+## 8. Normalization ----------------------------------------------------------------------------------------------------
+
+
+
+#' just a test still
+
+#' read las file
+#' it should have classified ground points:
+las <- readLAS("F:/Testdaten Befliegungen 2023/2017/ALS_2017_Sulzschachten.las")
+
+#' normalize point cloud based on DTM:
+las_normalized <- lidR::normalize_height(las, algorithm = tin())
+
+#' Step 1: Create a temporary variable to store the original Z values
+temp <- las_normalized$Z
+
+# Step 2: Switch Z and Zref
+las_normalized$Z <- las_normalized$Zref
+
+#' add the normalized Z values as a proper attribute for export:
+las_output <- add_lasattribute(las_normalized, x = temp, name = "NormalizedHeight", desc = "Height above ground")
+las_output@data
+#' add correct coordinate system:
+st_crs(las_output) <- 25832
+
+#' Visualize:
+plot(las_output, color = "Z")
+plot(las_output, color = "NormalizedHeight")
+
+#' export:
+writeLAS(las_output, "F:/Testdaten Befliegungen 2023/2017/ALS_2017_Sulzschachten_normalized.las")
+
+rm(las, las_normalized, las_output)
+gc()
+#' check:
+las_2 <- readLAS("H:/Testdaten Befliegungen 2023/Output/ALS_20230715_Sulzschachten.las")
+
