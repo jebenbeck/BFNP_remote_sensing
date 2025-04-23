19deba9e54ddc19a238f6b77deeebfbb75091867
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-04-23 08:43:07 +0200

    implemented reprojection of center point coordinates in single tree polygon data

--- Single tree polygon processing/Transform crs for single tree polygons.R
+++ Single tree polygon processing/Transform crs for single tree polygons.R
@@ -1,13 +1,13 @@
 library(sf)
+library(tidyverse)
 library(parallel)
 library(doParallel)
 library(foreach)
 
-
-
-files <- list.files("D:/Single tree polygons 2017/Projected_GK/", pattern = "*.gpkg$", recursive = T, full.names = TRUE)
+#' read in all the files that shall be processed:
+files <- list.files("E:/Single tree polygons 2017/01_Projected_GK/", pattern = "*.gpkg$", recursive = T, full.names = TRUE)
 filenames <- basename(files)
-
+layernames <- str_sub(filenames, start = 1, end = 8)
 
 #' Set up the parallel computation: 
 no_cores <- 8   #' number of cores
@@ -16,14 +16,36 @@ registerDoParallel(cl)
 
 
 foreach(i = 1:length(files), .packages = "sf") %dopar% {
-  #' read polygons
-  polygons <- read_sf(files[[i]], quiet = T)
-  #' convert projection to UTM:
-  polygons_prj <- st_transform(polygons, crs = st_crs("EPSG:25832"))
+  
+  #' read polygons and reproject them: to UTM Zone 32 N:
+  polygons_prj <- read_sf(files[[i]], quiet = T) %>% st_transform(crs = st_crs("EPSG:25832"))
+  
+  #' reproject center position of the trees to UTM:
+  centers_UTM <- st_drop_geometry(polygons_prj) %>% 
+    select(c(ID, X_POS, Y_POS)) %>%                          #' remove unnecessary columns to save memory 
+    st_as_sf(coords = c("X_POS", "Y_POS"), crs = 31468) %>%  #' transform to sf to be able to reproject data
+    st_transform(test_sf, crs = st_crs("EPSG:25832")) %>%    #' do the transformation
+    mutate(                                                  #' add the new coordinates to table as an attribute
+      X_POS = st_coordinates(.)[, "X"],
+      Y_POS = st_coordinates(.)[, "Y"]) %>% 
+    st_drop_geometry()
+
+  #' replace the coordinates in the polygon dataset:
+  polygons_prj_UTM <- polygons_prj %>%                    
+    left_join(centers_UTM, by = "ID", suffix = c("", "_new")) %>%  #' join the new columns with the polygon data
+    mutate(X_POS = X_POS_new, Y_POS = Y_POS_new) %>%               #' replace the coordinates
+    select(-X_POS_new, -Y_POS_new) %>%                             #' remove duplicate columns
+    arrange(ID) %>%                                                #' sort by ID
+    mutate(across(c(HEIGHT, TER_HEIGHT, CB_HEIGHT, CROWN_VOL), ~ round(.x, 2)))  #' round tree attribute values to cm
+
   #' export as gpkg:
-  st_write(polygons_prj, dsn = paste0("D:/Single tree polygons 2017/Projected_UTM/", filenames[[i]]), driver = "GPKG", append = F)
-  rm(polygons)
+  # st_write(polygons_prj, dsn = paste0("D:/Single tree polygons 2017/Projected_UTM/", filenames[[i]]), driver = "GPKG", append = F)
+  st_write(polygons_prj_UTM, dsn = "E:/Single tree polygons 2017/temp/NCUT_polygons_2017.gpkg", layer = paste0(layernames[[i]]), driver = "GPKG", append = T)
+  
+  #' clean data from memory:
   rm(polygons_prj)
+  rm(polygons_prj_UTM)
+  gc()
 }
 
 stopCluster(cl)
