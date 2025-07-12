7be35088724fe05cf639f3e214209a6269ada248
Jakob Ebenbeck <jakob.ebenbeck@npv-bw.bayern.de>
2025-07-12 11:01:53 +0200

    Updated 2023 data processing, updated normalization and dtm creation functions

--- ALS data preparation/Process ALS 2023.R
+++ ALS data preparation/Process ALS 2023.R
@@ -1,3 +1,41 @@
+## Info ----------------------------------------------------------------------------------------------------------------
+
+
+#' Author: Jakob Ebenbeck
+#' Last updated: 2025
+#' Status: Work in progress 
+
+
+### Purpose of script ----
+
+
+### Notes ----
+
+
+### Required datasets ----
+
+
+### Required packages ----
+
+library(lidR)
+library(sf)
+library(terra)
+library(mapview)
+library(future)
+library(tidyverse)
+library(stringr)
+library(ggplot2)
+library(yardstick)
+
+### Required functions and scripts ----
+
+source("ALS data preparation/Processing functions master.R")
+
+### Set working directories ----
+
+## 1. rename files -------------------------------------------------------------
+
+
 #' rename all the files according to the new nomenclature:
 
 # Load required library
@@ -38,3 +76,44 @@ for (old_path in files) {
   new_path <- rename_file(old_path)
   file.rename(old_path, new_path)
 }
+
+
+## 1. Classify ground points ---------------------------------------------------
+
+
+## 3. DTM creation -------------------------------------------------------------
+
+
+
+
+#' read in lascatalog:
+ctg <- readALSLAScatalog("G:/ALS 2023-07/pointclouds_classified")
+las_check(ctg)
+plot(ctg, mapview = T)
+
+#' perform the dtm creation:
+ctg_dtm <- catalog_dtm(ctg, output_path = "G:/ALS 2023-07/dtm", filename_convention = "{ORIGINALFILENAME}_dtm", mosaic_result = T,
+                       mosaic_name = "ALS_2023-07_DTM_Mosaic", parallel = T, n_cores = 10)
+
+
+
+
+
+## 4. Normalization ----------------------------------------------------------------------------------------------------------
+
+#' read in lascatalog:
+
+ctg <- readALSLAScatalog("G:/ALS 2023-07/pointclouds_classified")
+
+las_check(ctg)
+plot(ctg, mapview = T)
+
+#' perform the normalization with dtm:
+ctg_normalized_dtm <- catalog_normalize_dtm(ctg, dtm_path = "G:/ALS 2023-07/dtm/ALS_2023-07_DTM_Mosaic.tif", 
+      output_path = "G:/ALS 2023-07/pointclouds_normalized", "{ORIGINALFILENAME}", parallel = T, n_cores = 10)
+
+las_check(ctg)
+plot(ctg)
+
+
+
