3fbc9688d372ab6576141ce2b60395379ec2501e
Jakob Ebenbeck <jakob.ebenbeck@npv-bw.bayern.de>
2025-11-13 12:16:56 +0100

    update ALS 2019-2020 dataset

--- ALS data preparation/Process ALS 2019-2020.R
+++ ALS data preparation/Process ALS 2019-2020.R
@@ -1,82 +1,75 @@
 ## Info ----------------------------------------------------------------------------------------------------------------
 
 
-#' Author: Jakob Ebenbeck
-#' Last updated: 2025
-#' Status: Work in progress 
-
-
-### Purpose of script ----
-
-
-### Notes ----
-
-
-### Required datasets ----
+#' Harmonizing the ALS 2019-2020 dataset privided by the LDBV
 
 
 ### Required packages ----
 
+
 library(lidR)
-library(bfnpALSprocessor)
 library(sf)
 library(mapview)
 library(future)
 library(tidyverse)
 library(pbapply)
 library(stringr)
+library(bfnpALSprocessor)
 library(RCSF)
-library(yardstick)
+library(ggplot2)
 
 
 
-## 1. Retile the data --------------------------------------------------------------------------------------------------
+## 1. Retile to UTM32 grid ---------------------------------------------------------------------------------------------
 
 
-#' Catalog is retiled to assure filename consistency between the different datasets
-ctg <- readALSLAScatalog("G:/01_point_clouds/ALS 2019-2020/full_extent_original")
-crs(ctg) <- "EPSG:25832"
+#' Even though the tiling should be fine, catalog is retiled to assure filename consistency between the different datasets
 
-las_check(ctg)
-plot(ctg)
+#' read in lascatalog:
+ctg <- readALSLAScatalog("I:/01_point_clouds/ALS 2019-2020/full extent/00_original")
 
-ctg_retiled <- catalog_retile_template(lascatalog = ctg, output_path = "G:/01_point_clouds/ALS 2019-2020/full_extent_retiled")
+#' retile based on template
+ctg_retiled <- catalog_retile_template(lascatalog = ctg, output_path = "I:/01_point_clouds/ALS 2019-2020/full extent/01_retiled")
+
 
 
 ## 2. Normalization ----------------------------------------------------------------------------------------------------
 
-ctg_retiled <- readALScatalog("G:/01_point_clouds/ALS 2019-2020/full_extent_retiled")
-ctg_retiled
-
-crs(ctg_retiled) <- "epsg:25832"
-plot(ctg_retiled, mapview = T)
+ctg <- readALScatalog("I:/01_point_clouds/ALS 2019-2020/full extent/01_retiled")
 
 #' data gets normalized based on the official state DTM that was derived from this specific pointcloud: 
-ctg_normalized <- catalog_normalize(ctg_retiled, algorithm = "dtm", dtm_path = "G:/02_dtms/ALS 2019-2020/DTM1_Bayern_NPV_5km.tif", 
-                                    output_path = "G:/01_point_clouds/ALS 2019-2020/full_extent_normalized", parallel = T, n_cores = 12)
+ctg_normalized <- catalog_normalize(ctg, algorithm = "dtm", dtm_path = "I:/02_dtms/ALS 2019-2020/DTM1_Bayern_NPV_5km.tif", 
+                                    output_path = "I:/01_point_clouds/ALS 2019-2020/full extent/02_normalized", 
+                                    parallel = T, n_cores = 12)
+
+
+#' create lax files:
+lidR:::catalog_laxindex(ctg_normalized)
+
+
 
 ## 3. Export footprint polygons ----------------------------------------------------------------------------------------
 
 #' import lascatalog:
-ctg <- readALSLAScatalog("I:/01_point_clouds/ALS 2019-2020/full_extent_normalized")
+ctg <- readALSLAScatalog("I:/01_point_clouds/ALS 2019-2020/full_extent/02_normalized")
 
 #' make footprint polygons including statistics:
 ctg_statistics <- catalog_statistics(ctg, parallel = T, n_cores = 18, spatial = T)
-ctg_statistics
 
 #' export polygon file to geopackage:
 st_write(ctg_statistics, dsn = "I:/misc/ALS_tiles.gpkg", layer = "ALS 2019-2020", append = T)
 
 
 
-
-## 3. Clip AOIs --------------------------------------------------------------------------------------------------------
+## 4. Clip test areas --------------------------------------------------------------------------------------------------
 
 
 #' read AOIs:
 test_areas <- st_read("G:/misc/test_areas.gpkg", layer = "AOIs_UTM")
-mapview(test_areas)
 
-ctg_AOIs <- catalog_clip_polygons(ctg_normalized, input_epsg = "EPSG:25832", output_path = "G:/ALS 2007/pointclouds_test_AOIs/normalized",
+#' read in lascatalog:
+ctg <- readALSLAScatalog("I:/01_point_clouds/ALS 2019-2020/full_extent/02_normalized")
+
+ctg_AOIs <- catalog_clip_polygons(ctg, input_epsg = "EPSG:25832", output_path = "I:/01_point_clouds/ALS 2019-2020/test_areas", 
                                   filename_convention = "AOI_{name}", polygons = test_areas)
 
